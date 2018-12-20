6502dea7fe408755732bf53a2d24fe230729391a
Hillevi Eklöw <hillevieklow@gmail.com>
2018-12-20 17:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-function postComment() {
-  let commenter = document.getElementById('commenterName').value;
-  let comment = document.getElementById('commentText').value;
-  let commentTemplate = document.getElementById('comment-template').innerHTML;
-  let templateFn = _.template(commentTemplate);
-  var commentsSection = document.getElementById("comments");
-  commentsSection.innerHTML += templateFn({ 'commenter': commenterName, 'comment': commentText });
-}
-
 function createPost() {
-  let commentTemplate = _.template(document.getElementById('comment-template').innerHTML);
-  let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
-  let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
   var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById("postAuthor").value;
   var post = document.getElementById("postBody").value;
+
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
-  var section = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
-  var comments = commentsTemplate();
+
+  var blogSection = postTemplate({'title': postTitle, 'body': post, 'poster': postAuthor});
+  var commentsSection = commentsTemplate();
   var postElement = document.getElementById("post");
-  postElement.innerHTML = section;
-  postElement.getElementsByTagName("footer")[0].innerHTML = comments;
+
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
